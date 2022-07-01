ddc4741653d07e8a5eae2ce9f5e05486be928ded
Erica Wanja <wanjaerica@gmail.com>
2022-07-01 15:36:47 +0300

    responsive design

--- scripts/getProducts.js
+++ scripts/getProducts.js
@@ -1,7 +1,8 @@
-const getProducts = () => {
+
+const getProducts = async() => {
   // get token from localstorage
   const token = window.localStorage.getItem("token");
-  const products_table = document.getElementsByTagName("tbody")[0];
+
   const options = {
     method: "GET",
     headers: {
@@ -9,33 +10,60 @@ const getProducts = () => {
     },
   };
 
-  fetch(
+  await fetch(
     "https://cors.eu.org/https://api.demo.reja.ai/v1/product/list?business_id=253fc2e6-f4e3-454b-a7a5-d7144ce2758c",
     options
   )
     .then((response) => response.json())
     .then((response) => JSON.parse(JSON.stringify(response)))
     .then((data) => {
-      products_table.innerHTML = data.map((item) => {
-        return`
-        <tr>
-        <td>${item.name}</td>
-        <td>${item.price}</td>
-        <td>${item.units_available}</td>
-        </tr>
-        
-        `
-      }).join("");
+      products= data
+      some_data(data);
     })
 
     .catch((err) => console.error(err));
+};
+
+const some_data =  (data) => {
+ 
+  const products_table = document.getElementsByTagName("tbody")[0];
+  const data_list = [];
+  for (let i = 0; i < 10; i++) {
+    data_list.push(data[i]);
+  }
+  products_table.innerHTML = data_list.map((item) => {
+    return`
+    <tr>
+    <td>${item.name}</td>
+    <td>${item.price}</td>
+    <td>${item.units_available}</td>
+    </tr>
+    
+    `
+  }).join("");
+  console.log(data_list);
+};
+const showData = (products) => {
+  const products_table = document.getElementsByTagName("tbody")[0];
+  products_table.innerHTML = data
+    .map((item) => {
+      return `
+    <tr>
+    <td>${item.name}</td>
+    <td>${item.price}</td>
+    <td>${item.units_available}</td>
+    </tr>
+    
+    `;
+    })
+    .join("");
 };
 getProducts();
 
 const getSummary = async () => {
   // get token from localstorage
   const token = window.localStorage.getItem("token");
-  const summary_container = document.getElementById('data_summary')
+  const summary_container = document.getElementById("data_summary");
 
   const options = {
     method: "GET",
@@ -48,32 +76,30 @@ const getSummary = async () => {
     options
   )
     .then((response) => response.json())
-    
+
     .then((response) => JSON.parse(JSON.stringify(response)))
-    .then( (data) =>{
-      
-      summary_container.innerHTML= `
+    .then((data) => {
+      summary_container.innerHTML = `
       <div class="data_customer_details">
         <span><i class="bi bi-people"></i></span>
-        <div id='value' class="value">0</div>
+        <div id='value' class="value">${data.all_time.customers == null ? 0 : data.all_time.customers}</div>
         <div class="title">Customers</div>
     </div>
     <div class="data_transaction_details">
         <span><i class="bi bi-activity"></i></span>
-        <div id='value' class="value">${data.all_time.transactions}</div>
+        <div id='value' class="value">${data.all_time.transactions > 1000 ? data.all_time.transactions/1000 +'k': data.all_time.transactions}</div>
         <div class="title">Transactions</div>
     </div>
     <div class="data_sales_details">
         <span><i class="bi bi-receipt"></i></span>
-        <div id='value' class="value">${data.all_time.sales}</div>
+        <div id='value' class="value">${data.all_time.sales > 1000 ? data.all_time.sales/1000 +'k': data.all_time.sales}</div>
         <div class="title">Sales</div>
     </div>
-      `
+      `;
     })
     .catch((err) => console.error(err));
 };
 
 getSummary();
-
 
 
