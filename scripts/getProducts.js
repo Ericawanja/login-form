2d0ed882316302be462410fbce404091e9cfd2b1
Erica Wanja <wanjaerica@gmail.com>
2022-07-01 12:41:31 +0300

    table and summary details

--- scripts/getProducts.js
+++ scripts/getProducts.js
@@ -1,7 +1,7 @@
 const getProducts = () => {
   // get token from localstorage
   const token = window.localStorage.getItem("token");
-  const products_table = document.getElementsByTagName("table")[0];
+  const products_table = document.getElementsByTagName("tbody")[0];
   const options = {
     method: "GET",
     headers: {
@@ -32,9 +32,10 @@ const getProducts = () => {
 };
 getProducts();
 
-const getSummary = () => {
+const getSummary = async () => {
   // get token from localstorage
   const token = window.localStorage.getItem("token");
+  const summary_container = document.getElementById('data_summary')
 
   const options = {
     method: "GET",
@@ -42,16 +43,37 @@ const getSummary = () => {
       Authorization: `Bearer ${token}`,
     },
   };
-  fetch(
+  await fetch(
     "https://cors.eu.org/https://api.demo.reja.ai/v1/data/summary/?business_id=253fc2e6-f4e3-454b-a7a5-d7144ce2758c",
     options
   )
     .then((response) => response.json())
-    .then((response) => console.log(response))
+    
+    .then((response) => JSON.parse(JSON.stringify(response)))
+    .then( (data) =>{
+      
+      summary_container.innerHTML= `
+      <div class="data_customer_details">
+        <span><i class="bi bi-people"></i></span>
+        <div id='value' class="value">0</div>
+        <div class="title">Customers</div>
+    </div>
+    <div class="data_transaction_details">
+        <span><i class="bi bi-activity"></i></span>
+        <div id='value' class="value">${data.all_time.transactions}</div>
+        <div class="title">Transactions</div>
+    </div>
+    <div class="data_sales_details">
+        <span><i class="bi bi-receipt"></i></span>
+        <div id='value' class="value">${data.all_time.sales}</div>
+        <div class="title">Sales</div>
+    </div>
+      `
+    })
     .catch((err) => console.error(err));
 };
+
 getSummary();
 
-const mapData = (data) => {
-  console.log(data);
-};
+
+
